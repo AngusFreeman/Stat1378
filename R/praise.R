fe2adfccc3874d9f6deb8d7b4e209204e43e12d1
Angus Freeman <angusafreeman@gmail.com>
2021-09-07 16:00:11 +1000

    praise function

--- R/praise.R
+++ R/praise.R
@@ -1,3 +1,12 @@
+#' Title
+#'
+#' @param name
+#' @param punctuation
+#'
+#' @return
+#' @export
+#'
+#' @examples
 praise <- function(name, punctuation = "!"){
   glue::glue("You're the best, {name}{punctuation}")
 }
